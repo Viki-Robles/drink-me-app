516ddb257792ebd89448492289abe40997f090c7
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.default>
2020-06-02 18:32:59 +0100

    add the Main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Main from "./Components/Main/Main.js";
 
 function App() {
   return (
     <div className="App">
-    
+      <Main/>
     </div>
   );
 }
