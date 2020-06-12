72c88cfd59059c57592e867b860dd841f2bb7b30
Aaron Hawthorne <aaronhawthorne12345@gmail.com>
2020-06-12 17:12:29 +0100

    updating fork

--- src/TeamMember/TeamMember.js
+++ src/TeamMember/TeamMember.js
@@ -5,10 +5,12 @@ export default function TeamMember(props) {
     return (
         <div className="TeamMember">
             <a href={props.linkedIn} target="_blank" rel="noopener noreferrer">
+            <a href={props.github} target="_blank" rel="noopener noreferrer">
                 <div className="teamContainer">
                     <img src={props.teamImage} alt={`${props.name}`} />
                     <div className="teamMemberName">{props.name}</div>
                 </div>
+                </a>
             </a>
         </div>
     );
