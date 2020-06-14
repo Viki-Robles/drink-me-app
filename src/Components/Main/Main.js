9b3e9eac9b08a74f28565a3878fdd2f02d4ffbb8
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-14 23:49:34 +0100

    Sort Drop Down list Alphabetically

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,7 +4,6 @@ import "./Main.css";
 import Results from "../Results/Results.js";
 import { Link } from "react-router-dom";
 
-
 export default function Main() {
   const [listIngredients, setListIngredients] = useState([]);
   const [ingredient, setIngredient] = useState("");
@@ -25,9 +24,9 @@ export default function Main() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then((response) => response.json())
       .then((json) => {
-        setRandom(json.drinks)
+        setRandom(json.drinks);
       });
-  }
+  };
 
   if (!isLoaded) {
     return <div>Loadin Main...</div>;
@@ -44,46 +43,61 @@ export default function Main() {
           <div className="container-text">
             <p>
               <span className="container-logo">DrinkMeApp</span>
-              is an application where you can be creative at home on your own space and experiment
-              the different type of coctails and drinks that are
-              available on our "DYI" recipe guide.
+              is an application where you can be creative at home on your own
+              space and experiment the different type of coctails and drinks
+              that are available on our "DYI" recipe guide.
               <br />
             </p>
           </div>
-          <p className="container-p">Please use the buttons below to choose an ingredient or if you feel is 
-          your "lucky" day choose the "lucky button" for inspiration.</p>
+          <p className="container-p">
+            Please use the buttons below to choose an ingredient or if you feel
+            is your "lucky" day choose the "lucky button" for inspiration.
+          </p>
           <div className="container-buttons">
-          <div className="main-picture-container">
+            <div className="main-picture-container">
               <img src={martini} alt="martini" />
             </div>
             <div className="container-buttons-boxes">
               <div>
-                <button className="button-random"
-                  onClick={() => fetchRandomData(!buttonOn)}>Feeling lucky
+                <button
+                  className="button-random"
+                  onClick={() => fetchRandomData(!buttonOn)}
+                >
+                  Feeling lucky
                 </button>
                 <div className="random-container">
-                  {
-                    listRandom.map((drink) => (
-                      <div key={drink.idDrink}>
-                        <p className="random-container-p">Checkout this one!</p>
-                        <Link to={`/drink/${drink.idDrink}`}>
+                  {listRandom.map((drink) => (
+                    <div key={drink.idDrink}>
+                      <p className="random-container-p">Checkout this one!</p>
+                      <Link to={`/drink/${drink.idDrink}`}>
                         <div className="random-container-picture">
-                          <div className="random-container-title">{drink.strDrink}</div>
+                          <div className="random-container-title">
+                            {drink.strDrink}
+                          </div>
                           <img src={drink.strDrinkThumb} />
                         </div>
-                        </Link>
-                      </div>
-                    ))
-                  }
+                      </Link>
+                    </div>
+                  ))}
                 </div>
               </div>
               <form className="container-form">
-                <select onChange={(e) => setIngredient(e.target.value)} className="container-select">
-                  {listIngredients.map((drink) => (
-                    <option key={drink.strIngredient1} value={drink.strIngredient1}>
-                      {drink.strIngredient1}
-                    </option>
-                  ))}
+                <select
+                  onChange={(e) => setIngredient(e.target.value)}
+                  className="container-select"
+                >
+                  {listIngredients
+                    .sort((drinkA, drinkB) =>
+                      drinkA.strIngredient1 > drinkB.strIngredient1 ? 1 : -1
+                    )
+                    .map((drink) => (
+                      <option
+                        key={drink.strIngredient1}
+                        value={drink.strIngredient1}
+                      >
+                        {drink.strIngredient1}
+                      </option>
+                    ))}
                 </select>
               </form>
             </div>
