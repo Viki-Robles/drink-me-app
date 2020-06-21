d409e5bcf9362797dc8fa443b787ba158c8f76db
Vicky Vasilopoulou <vickyvasilopoulou@vickys-macbook.lan>
2020-06-21 17:28:15 +0100

    changing layout of the App

---
+++ src/Components/MainText/MainText.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MainText() {
+    return (
+        <p>
+            Have you ever been in a
+            situation where the sun is shining<br />
+            and all you want is a cocktail to cool you down?<br />
+            <br />Me too! We have made it easy for you to decide what cocktail to
+            make.<br /><br />Click on an ingredient below and voila, hey presto you have a
+            selection to choose from.<br /><br />Not sure on how to make it?
+            No worries, click on your cocktail of
+            choice and we’ll help you through making it!
+        </p>
+    );
+}
